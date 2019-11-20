f9b1512f1200308480e0a183d69ac5aa291d00c2
b2 <b2@Ter72-W10.cen.local>
2019-11-19 21:01:31 -0300

    primera version

---
+++ src/saboresiara/SaboresIara.java
@@ -0,0 +1,14 @@
+package saboresiara;
+
+public class SaboresIara {
+
+    public static void main(String[] args) {
+        String[] sabores = {"Dulce", "Salado", "Amargo", "Acido", "Umami", "Picante", "Astringente", "Starchy", "Adiposo"};
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Sabor:" + sabores[i]);
+
+        }
+
+    }
+
+}
